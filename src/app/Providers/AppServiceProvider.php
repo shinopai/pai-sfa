c5027b0eb60d89cb9c229c53d5291cf77ebd63fb
shinopai <harbor.shinozaki@gmail.com>
2026-08-06 15:33:21 +0900

    fix: Mixed Contentエラー防止のため本番環境でHTTPS接続を強制

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
         // ページネーション
         Paginator::defaultView('vendor.pagination.pai');
         Paginator::defaultSimpleView('vendor.pagination.pai');
+
+        // 本番環境（またはHTTPSリクエスト時）で生成されるURLをhttpsに強制する
+        if (config('app.env') === 'production' || request()->header('X-Forwarded-Proto') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
