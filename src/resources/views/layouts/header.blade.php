832e68dda066c285747dcbf51dcf3d966a92e009
shinopai <harbor.shinozaki@gmail.com>
2026-08-07 09:07:32 +0900

    refactor: ヘッダーロゴをpublic/imagesに移動しassetヘルパー参照に変更

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
   <div class="header__inner u-wrap u-flex">
     <a class="header__logo" href="{{ route('dashboard') }}">
-      <img src="{{ Vite::asset('resources/images/logo/pai-sfa-logo.svg') }}" alt="PaiSFA">
+      <img src="{{ asset('images/logo/pai-sfa-logo.svg') }}" alt="PaiSFA">
     </a>
 
     <nav class="header__nav u-flex" aria-label="メニュー">
